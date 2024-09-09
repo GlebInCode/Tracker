bedd21d3c3d28f334562be71a5fcae4b0bbddaee
Khamin Gleb <nimik19@yandex.ru>
2024-09-09 23:07:01 +0300

    Реализована архитектура MVVM в списке категорий

---
+++ Tracker/Tracker/CreatingTracker/CategoryTrackerViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  CategoryTrackerViewModel.swift
+//  Tracker
+//
+//  Created by Глеб Хамин on 09.09.2024.
+//
+
+import Foundation
+
+final class CategoryTrackerViewModel {
+    
+    // MARK: - Public Properties
+
+    var categories: [TrackerCategory] = []
+    var selectedCategory: String?
+    
+    // MARK: - Private Properties
+        
+    private let trackerCategoryStore = TrackerCategoryStore()
+    
+    // MARK: - Initialization
+    
+    init() {
+        updateCategory()
+        loadSelectedCategory()
+    }
+    
+    // MARK: - Table
+    
+    func countCategory() -> Int {
+        categories.count
+    }
+    
+    func titleCell(_ indexRow: Int) -> String {
+        categories[indexRow].title
+    }
+    
+    func selectedCell(_ title: String) -> Bool {
+        title == selectedCategory
+    }
+    
+    func didSelectCategory(_ title: String) {
+        selectedCategory = title
+        saveSelectedCategory()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func saveSelectedCategory() {
+        guard let selectedCategory else {
+            return
+        }
+        UserDefaults.standard.set(
+            selectedCategory,
+            forKey: Constants.selectedCategory)
+    }
+    
+    private func loadSelectedCategory() {
+        guard let title = UserDefaults.standard.string(
+            forKey: Constants.selectedCategory
+        ) else { return }
+        selectedCategory = title
+    }
+    
+    private func updateCategory() {
+        do {
+            categories = try trackerCategoryStore.getCategory()
+        } catch {
+            print("Данные не доступны")
+        }
+    }
+}
---
+++ Tracker/Tracker/Helpers/Binding.swift
@@ -0,0 +1,10 @@
+//
+//  Binding.swift
+//  Tracker
+//
+//  Created by Глеб Хамин on 09.09.2024.
+//
+
+import Foundation
+
+typealias Binding<T> = (T) -> Void
